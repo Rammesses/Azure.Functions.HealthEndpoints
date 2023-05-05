f65ebb8fca7a5063526c566efe0592d3ba92ea5d
Joel Hammond-Turner <joel.hammond-turner@landmark.co.uk>
2023-05-05 13:14:57 +0100

    Updated PPTX, plus minor code cleanups

--- Samples/Azure.FunctionApp/WeatherForecastFunction.cs
+++ Samples/Azure.FunctionApp/WeatherForecastFunction.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,7 +6,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
